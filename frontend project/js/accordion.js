8b5ffd6819f6ae23e683ac32e94bf7720dfefa30
Orkhan770 <orkhanmm@code.edu.az>
2020-07-21 18:06:38 +0400

    accordion1

--- frontend project/js/accordion.js
+++ frontend project/js/accordion.js
@@ -1,16 +1,61 @@
 $(document).ready(function() {
 
-    $(document).on("click", ".card-header", function() {
-        if ($(this).next()[0] != $(".active")[0]) {
-            $(".active").slideUp("slow", function() {
-                $(this).removeClass("active")
+    $(document).on("click", ".card-header1", function() {
+        if ($(this).next()[0] != $(".active1")[0]) {
+            $(".active1").slideUp("slow", function() {
+                $(this).removeClass("active1")
             });
             $(this).next().slideDown("slow", function() {
-                $(this).addClass("active")
+                $(this).addClass("active1")
             })
         } else {
-            $(".active").slideUp("slow", function() {
-                $(this).removeClass("active")
+            $(".active1").slideUp("slow", function() {
+                $(this).removeClass("active1")
+            });
+        }
+    })
+
+    $(document).on("click", ".card-header2", function() {
+        if ($(this).next()[0] != $(".active2")[0]) {
+            $(".active2").slideUp("slow", function() {
+                $(this).removeClass("active2")
+            });
+            $(this).next().slideDown("slow", function() {
+                $(this).addClass("active2")
+            })
+        } else {
+            $(".active2").slideUp("slow", function() {
+                $(this).removeClass("active2")
+            });
+        }
+    })
+
+    $(document).on("click", ".card-header3", function() {
+        if ($(this).next()[0] != $(".active3")[0]) {
+            $(".active3").slideUp("slow", function() {
+                $(this).removeClass("active3")
+            });
+            $(this).next().slideDown("slow", function() {
+                $(this).addClass("active3")
+            })
+        } else {
+            $(".active3").slideUp("slow", function() {
+                $(this).removeClass("active3")
+            });
+        }
+    })
+
+    $(document).on("click", ".card-header4", function() {
+        if ($(this).next()[0] != $(".active4")[0]) {
+            $(".active4").slideUp("slow", function() {
+                $(this).removeClass("active4")
+            });
+            $(this).next().slideDown("slow", function() {
+                $(this).addClass("active4")
+            })
+        } else {
+            $(".active4").slideUp("slow", function() {
+                $(this).removeClass("active4")
             });
         }
     })
